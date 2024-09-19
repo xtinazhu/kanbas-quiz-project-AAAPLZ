34ecad344eb7ce16d50f783028be8b769edbf8e1
evazhuyh <zhu.yihua@northeastern.edu>
2024-09-19 16:05:37 -0400

    kanbas 3.5

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,4 +1,5 @@
 import CoursesNavigation from "./Navigation";
+import Modules from "./Modules";
 import { Navigate, Route, Routes } from "react-router";
 export default function Courses() {
   return (
@@ -13,11 +14,11 @@ export default function Courses() {
           <td valign="top">
             <Routes>
               <Route path="/" element={<Navigate to="Home" />} />
-              <Route path="Home" element={<h2>Home</h2>} />
-              <Route path="Modules" element={<h2>Modules</h2>} />
-              <Route path="Assignments" element={<h2>Assignments</h2>} />
-              <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>} />
-              <Route path="People" element={<h2>People</h2>} />
+              <Route path="Home" element={<h3>Home</h3>} />
+              <Route path="Modules" element={<Modules />} />
+              <Route path="Assignments" element={<h3>Assignments</h3>} />
+              <Route path="Assignments/:aid" element={<h3>Assignment Editor</h3>} />
+              <Route path="People" element={<h3>People</h3>} />
             </Routes>
           </td>
         </tr>
