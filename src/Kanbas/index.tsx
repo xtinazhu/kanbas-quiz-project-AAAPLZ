2785acfdf4f5b02fdd02f27ef9f37efdc8b451da
evazhuyh <zhu.yihua@northeastern.edu>
2024-09-19 15:56:39 -0400

    kanbas 3.4

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router";
 import Account from "./Account";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
+import Courses from "./Courses";
 export default function Kanbas() {
   return (
     <div id="wd-kanbas">
@@ -11,10 +12,13 @@ export default function Kanbas() {
             <KanbasNavigation />
           </td>
           <td valign="top">
-            <Routes>
-              <Route path="/" element={<Navigate to="/Kanbas/Account" />} />
+          <Routes>
+              <Route path="/" element={<Navigate to="Account" />} />
               <Route path="/Account/*" element={<Account />} />
               <Route path="/Dashboard" element={<Dashboard />} />
+              <Route path="/Courses/:cid/*" element={<Courses />} />
+              <Route path="/Calendar" element={<h1>Calendar</h1>} />
+              <Route path="/Inbox" element={<h1>Inbox</h1>} />
             </Routes>
           </td>
         </tr>
