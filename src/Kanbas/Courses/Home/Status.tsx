8db728b49bf68f4ed8f2ae5f7051952f39c6ae94
evazhuyh <zhu.yihua@northeastern.edu>
2024-09-19 16:55:18 -0400

    kanbas 3.6 Home Course Status

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -2,8 +2,15 @@ export default function CourseStatus() {
   return (
     <div id="wd-course-status">
       <h2>Course Status</h2>
-      <button>Unpublish</button> <button>Publish</button>
-      {/* Complete on your own */}
-      <button>View Course Notifications</button>
+      <button>
+        Unpublish</button> <button>
+        Publish</button> <button>
+        Import Existing Content</button> <button>
+        Import from Commons</button> <button>
+        Choose Home Page</button> <button>
+        View Course Stream</button> <button>
+        New Announcement</button> <button>
+        New Analytics</button> <button>
+        View Course Notifications</button>
     </div>
 );}
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,5 +1,6 @@
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
+import Home from "./Home";
 import { Navigate, Route, Routes } from "react-router";
 export default function Courses() {
   return (
@@ -14,7 +15,7 @@ export default function Courses() {
           <td valign="top">
             <Routes>
               <Route path="/" element={<Navigate to="Home" />} />
-              <Route path="Home" element={<h3>Home</h3>} />
+              <Route path="Home" element={<Home />} />
               <Route path="Modules" element={<Modules />} />
               <Route path="Assignments" element={<h3>Assignments</h3>} />
               <Route path="Assignments/:aid" element={<h3>Assignment Editor</h3>} />
