d022e4b1d428a3f9b1256863178b38bbabac0fe7
evazhuyh <zhu.yihua@northeastern.edu>
2024-09-19 15:00:05 -0400

    kanbas 3.2.1

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,6 +1,9 @@
+import { Routes, Route, Navigate } from "react-router";
+import Account from "./Account";
 export default function Kanbas() {
   return (
     <div id="wd-kanbas">
       <h1>Kanbas</h1>
+      <Account />
     </div>
 );}
