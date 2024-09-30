f209c66f16cc00353dc860d3e34ebd80306b49fc
evazhuyh <zhu.yihua@northeastern.edu>
2024-09-30 01:16:26 -0400

    a2 3.1.2

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,7 +1,30 @@
+/*export default function Lab2() {
+  return (
+    <div id="wd-lab2">
+      <h2>Lab 2 - Cascading Style Sheets</h2>
+      <h3>Styling with the STYLE attribute</h3>
+      <p style={{ backgroundColor: "blue",
+                  color: "white" }}>
+        Style attribute allows configuring look and feel
+        right on the element. Although it's very convenient
+        it is considered bad practice and you should avoid
+        using the style attribute
+      </p>
+    </div>
+  );
+}*/
+import "./index.css";
 export default function Lab2() {
   return (
-    <div>
-      <h2>Lab 2</h2>
+    <div id="wd-lab2">
+      <h2>Lab 2 - Cascading Style Sheets</h2>
+      <h3>Styling with the STYLE attribute</h3>
+      <p>
+        Style attribute allows configuring look and feel
+        right on the element. Although it's very convenient
+        it is considered bad practice and you should avoid
+        using the style attribute
+      </p>
     </div>
   );
 }
