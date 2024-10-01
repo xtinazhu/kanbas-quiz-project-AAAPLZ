baf992bf89a10abd8ae71dc5c9d6ed3c5a5cd127
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-01 16:46:36 -0400

    a2 3.1.3

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -13,6 +13,7 @@
     </div>
   );
 }*/
+
 import "./index.css";
 export default function Lab2() {
   return (
@@ -25,6 +26,17 @@ export default function Lab2() {
         it is considered bad practice and you should avoid
         using the style attribute
       </p>
+      <div id="wd-css-id-selectors">
+        <h3>ID selectors</h3>
+        <p id="wd-id-selector-1">
+Instead of changing the look and feel of all the 
+elements of the same name, e.g., P, we can refer to a specific element by its ID
+        </p>
+        <p id="wd-id-selector-2">
+Here's another paragraph using a different ID and a different look and
+          feel
+        </p>
+      </div>
     </div>
   );
 }
