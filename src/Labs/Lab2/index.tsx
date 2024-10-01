d4cf657e70991091725a45c794f774dfd5ce0507
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-01 17:56:24 -0400

    a2 3.1.5

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -38,7 +38,7 @@ Here's another paragraph using a different ID and a different look and
           feel
         </p>
       </div>
-      
+
       <div id="wd-css-class-selectors">
         <h3>Class selectors</h3>
         <p className="wd-class-selector">
@@ -47,6 +47,27 @@ Here's another paragraph using a different ID and a different look and
         <h4 className="wd-class-selector">
       This heading has same style as paragraph above
         </h4>
+      </div>
+
+      <div id="wd-css-document-structure">
+        <div className="wd-selector-1">
+          <h3>Document structure selectors</h3>
+          <div className="wd-selector-2">
+            Selectors can be combined to refer elements in particular
+            places in the document
+            <p className="wd-selector-3">
+              This paragraph's red background is referenced as
+              <br />
+              .selector-2 .selector3<br />
+              meaning the descendant of some ancestor.<br />
+              <span className="wd-selector-4">
+                Whereas this span is a direct child of its parent
+              </span><br />
+                You can combine these relationships to create specific 
+                styles depending on the document structure
+            </p>
+          </div>
+        </div>
       </div>
 
     </div>
