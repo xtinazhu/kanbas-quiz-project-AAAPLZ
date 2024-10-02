0e63d8d0d5a2e3ac46625861b20dbd28fb40b343
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-02 14:18:13 -0400

    a2 3.1.7

---
+++ src/Labs/Lab2/ForegroundColors.tsx
@@ -0,0 +1,8 @@
+<div id="wd-css-colors">
+  <h2>Colors</h2>
+  <h3 className="wd-fg-color-blue">Foreground color</h3>
+  <p className="wd-fg-color-red">
+    The text in this paragraph is red but
+    <span className="wd-fg-color-green">this text is green</span>
+  </p>
+</div>
