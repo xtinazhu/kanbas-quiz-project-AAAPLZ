d996917b56184b12a8073eaf54af95c27b591b09
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-02 19:57:26 -0400

    a2 3.3.13

--- src/Labs/Lab2/BootstrapForms.tsx
+++ src/Labs/Lab2/BootstrapForms.tsx
@@ -58,5 +58,14 @@ export default function BootstrapForms() {
           </div>
         </div>
 
+        <div id="wd-css-styling-range-and-sliders"> {/* Range and Sliders */}
+          <h3>Range</h3>
+          <label htmlFor="range1" className="form-label">
+            Example range
+          </label>
+          <input type="range" className="form-range"
+            min="0" max="5" step="0.5" id="range1" />
+        </div>
+
       </div>
 );}
