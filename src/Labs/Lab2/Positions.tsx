44a386a8c3aadf9442eeff26c742ff99865a7201
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-02 15:39:49 -0400

    a2 3.1.15

--- src/Labs/Lab2/Positions.tsx
+++ src/Labs/Lab2/Positions.tsx
@@ -32,5 +32,16 @@ export default function Dimensions() {
             Square</div>
         </div><br /><br /><br /><br /><br /><br /><br />
       </div>
+      
+      <div id="wd-css-position-fixed">
+        <h2>Fixed position</h2>
+        Checkout the blue square that says "Fixed position" stuck all the way on the right and half way down the page. It doesn't scroll with the rest of the page. Its position is "Fixed".
+        <div className="wd-pos-fixed 
+          wd-dimension-square wd-bg-color-blue 
+          wd-fg-color-white">
+          Fixed position
+        </div>
+      </div>
+
     </div>
 );}
