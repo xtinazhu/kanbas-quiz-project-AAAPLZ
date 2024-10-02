f65a2e771db9b4151aef80bfebfd6e3564bf5f80
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-02 15:37:37 -0400

    a2 3.1.14

--- src/Labs/Lab2/Positions.tsx
+++ src/Labs/Lab2/Positions.tsx
@@ -1,18 +1,36 @@
 export default function Dimensions() {
   return (
-    <div id="wd-css-position-relative">
-      <h2>Relative</h2>
-      <div className="wd-bg-color-gray">
-        <div className="wd-bg-color-yellow 
-                        wd-dimension-portrait">
-          <div className="wd-pos-relative-nudge-down-right">
-            Portrait</div></div>
-        <div className="wd-pos-relative-nudge-up-right 
-            wd-bg-color-blue wd-fg-color-white 
-            wd-dimension-landscape">
-          Landscape</div>
-        <div className="wd-bg-color-red wd-dimension-square">
-          Square</div>
+    <div>
+      <div id="wd-css-position-relative">
+        <h2>Relative</h2>
+        <div className="wd-bg-color-gray">
+          <div className="wd-bg-color-yellow 
+                          wd-dimension-portrait">
+            <div className="wd-pos-relative-nudge-down-right">
+              Portrait</div></div>
+          <div className="wd-pos-relative-nudge-up-right 
+              wd-bg-color-blue wd-fg-color-white 
+              wd-dimension-landscape">
+            Landscape</div>
+          <div className="wd-bg-color-red wd-dimension-square">
+            Square</div>
+        </div>
+      </div>
+
+      <div id="wd-css-position-absolute">
+        <h2>Absolute position</h2>
+        <div className="wd-pos-relative">
+          <div className="wd-pos-absolute-10-10 
+              wd-bg-color-yellow wd-dimension-portrait">
+            Portrait</div>
+          <div className="wd-pos-absolute-50-50 
+              wd-bg-color-blue wd-fg-color-white 
+              wd-dimension-landscape">
+            Landscape</div>
+          <div className="wd-pos-absolute-120-20 
+              wd-bg-color-red wd-dimension-square">
+            Square</div>
+        </div><br /><br /><br /><br /><br /><br /><br />
       </div>
     </div>
 );}
