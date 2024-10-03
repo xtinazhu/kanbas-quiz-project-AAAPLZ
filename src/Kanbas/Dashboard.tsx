838dc5a1ddd97c672274acbd606cc8dd0e3c5db5
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-03 13:24:49 -0400

    a2 4.3

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -3,7 +3,7 @@ export default function Dashboard() {
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+      <h2 id="wd-dashboard-published">Published Courses (7)</h2> <hr />
       <div id="wd-dashboard-courses" className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
           <div className="wd-dashboard-course col" style={{ width: "300px" }}>
