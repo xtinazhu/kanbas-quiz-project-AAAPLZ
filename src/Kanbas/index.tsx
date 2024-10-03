4cd65e52768bdd771905e6264089fd77c7f74121
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-02 22:22:50 -0400

    a2 4.1

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,3 +1,4 @@
+import "./styles.css";
 import { Routes, Route, Navigate } from "react-router";
 import Account from "./Account";
 import Dashboard from "./Dashboard";
@@ -6,13 +7,13 @@ import Courses from "./Courses";
 export default function Kanbas() {
   return (
     <div id="wd-kanbas">
-      <h1>Yihua Zhu 20593</h1>
-      <h3>GitHub: https://a1--1-kanbas-react-web-app-a1.netlify.app/#/Kanbas/Account/Signin</h3>
-      <h1>Kanbas</h1>
       <KanbasNavigation />
-        <div>
+        <div className="wd-main-content-offset p-3">
+          <h1>Yihua Zhu 20593</h1>
+          <h3>GitHub: https://a1--1-kanbas-react-web-app-a1.netlify.app/#/Kanbas/Account/Signin</h3>
+          <h1>Kanbas</h1>
           <Routes>
-              <Route path="/" element={<Navigate to="Account" />} />
+              <Route path="/" element={<Navigate to="Dashboard" />} />
               <Route path="/Account/*" element={<Account />} />
               <Route path="/Dashboard" element={<Dashboard />} />
               <Route path="/Courses/:cid/*" element={<Courses />} />
