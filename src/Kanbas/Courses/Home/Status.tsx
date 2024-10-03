f92702b76c1d925a50f2327ac1f493539b77a996
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-03 15:07:58 -0400

    a2 4.5

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -1,16 +1,41 @@
+import { MdDoNotDisturbAlt } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
+import { BiImport } from "react-icons/bi";
+import { LiaFileImportSolid } from "react-icons/lia";
+import { BiTargetLock } from "react-icons/bi";
+import { FaChartSimple } from "react-icons/fa6";
+import { GrAnnounce } from "react-icons/gr";
+import { FaChartLine } from "react-icons/fa6";
+import { FiBell } from "react-icons/fi";
 export default function CourseStatus() {
   return (
-    <div id="wd-course-status">
-      <h2>Course Status</h2>
-      <button>
-        Unpublish</button> <button>
-        Publish</button> <button>
-        Import Existing Content</button> <button>
-        Import from Commons</button> <button>
-        Choose Home Page</button> <button>
-        View Course Stream</button> <button>
-        New Announcement</button> <button>
-        New Analytics</button> <button>
-        View Course Notifications</button>
+    <div className="wd-margin-right-left wd-padded-bottom-right wd-border-fat">
+      <div id="wd-course-status" style={{ width: "300px" }}>
+        <h2>Course Status</h2>
+        <div className="d-flex">
+          <div className="w-50 pe-1">
+            <button className="btn btn-lg btn-secondary w-100 text-nowrap ">
+              <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish </button>
+          </div>
+          <div className="w-50">
+            <button className="btn btn-lg btn-success w-100">
+              <FaCheckCircle className="me-2 fs-5" /> Publish </button>
+          </div>
+        </div><br />
+        <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+          <BiImport className="me-2 fs-5" /> Import Existing Content </button>
+        <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+          <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons </button>
+        <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+          <BiTargetLock className="me-2 fs-5" /> Choose Home Page </button>
+        <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+          <FaChartSimple className="me-2 fs-5" /> View Course Stream </button>
+        <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+          <GrAnnounce className="me-2 fs-5" /> New Announcement </button>
+        <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+          <FaChartLine className="me-2 fs-5" /> New Analytics </button>
+        <button className="btn btn-lg btn-secondary w-100 mt-1 text-start">
+          <FiBell className="me-2 fs-5" /> View Course Notifications </button>
+      </div>
     </div>
 );}
