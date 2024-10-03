93e09d4fbcd737585b1abbf9edcd0b53674252d2
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-02 22:44:31 -0400

    a2 4.2

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -4,119 +4,135 @@ export default function Dashboard() {
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/1234/Home">
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5>
-                 CS1234 React JS {/* Course 1 */}
-              </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer
-              </p>
-              <button> Go </button>
+      <div id="wd-dashboard-courses" className="row">
+        <div className="row row-cols-1 row-cols-md-5 g-4">
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-3 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/1234/Home">
+                <img src="/images/reactjs.jpg" width="100%" height={160}/>
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS1234 React JS
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                      Full Stack software developer
+                  </p>
+                  <button className="btn btn-primary"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
+          </div>
 
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/5010/Home">
-            <img src="/images/5010.jpg" width={200} />
-            <div>
-              <h5>
-                 CS5010 Programming Design Paradigm	{/* Course 2 */}
-              </h5>
-              <p className="wd-dashboard-course-title">
-                Modern program design paradigms
-              </p>
-              <button> Go </button>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-3 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/5010/Home">
+                <img src="/images/5010.jpg" width="100%" height={160}/>
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                    CS5010 Programming Design Paradigm	{/* Course 2 */}
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                    Modern program design paradigms
+                  </p>
+                  <button className="btn btn-primary"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
+          </div>
 
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/5800/Home">
-            <img src="/images/5800.jpg" width={200} />
-            <div>
-              <h5>
-                 CS5800 Algorithms {/* Course 3 */}
-              </h5>
-              <p className="wd-dashboard-course-title">
-                The mathematical techniques used for computer algorithms
-              </p>
-              <button> Go </button>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-3 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/5800/Home">
+                <img src="/images/5800.jpg" width="100%" height={160}/>
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                  CS5800 Algorithms	{/* Course 3 */}
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                  The mathematical techniques used for computer algorithms
+                  </p>
+                  <button className="btn btn-primary"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
+          </div>
 
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/5400/Home">
-            <img src="/images/5400.jpg" width={200} />
-            <div>
-              <h5>
-                 CS5400 Principles of Programming Language {/* Course 4 */}
-              </h5>
-              <p className="wd-dashboard-course-title">
-                The basic components of programming languages
-              </p>
-              <button> Go </button>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-3 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/5400/Home">
+                <img src="/images/5400.jpg" width="100%" height={160}/>
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                  CS5400 Principles of Programming Language	{/* Course 4 */}
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                  The basic components of programming languages
+                  </p>
+                  <button className="btn btn-primary"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
+          </div>
 
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/5500/Home">
-            <img src="/images/5500.jpg" width={200} />
-            <div>
-              <h5>
-                 CS5500 Foundations of Software Engineering {/* Course 5 */}
-              </h5>
-              <p className="wd-dashboard-course-title">
-                Software development life cycle models
-              </p>
-              <button> Go </button>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-3 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/5500/Home">
+                <img src="/images/5500.jpg" width="100%" height={160}/>
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                  CS5500 Foundations of Software Engineering	{/* Course 5 */}
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                  Software development life cycle models
+                  </p>
+                  <button className="btn btn-primary"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
+          </div>
 
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/5520/Home">
-            <img src="/images/5520.jpg" width={200} />
-            <div>
-              <h5>
-                 CS5520 Mobile Application Development {/* Course 6 */}
-              </h5>
-              <p className="wd-dashboard-course-title">
-                Mobile application development on mobile phones
-              </p>
-              <button> Go </button>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-3 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/5520/Home">
+                <img src="/images/5520.jpg" width="100%" height={160}/>
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                  CS5520 Mobile Application Development	{/* Course 6 */}
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                  Mobile application development on mobile phones
+                  </p>
+                  <button className="btn btn-primary"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
-        </div>
+          </div>
 
-        <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/5600/Home">
-            <img src="/images/5600.jpg" width={200} />
-            <div>
-              <h5>
-                 CS5600 Computer Systems {/* Course 7 */}
-              </h5>
-              <p className="wd-dashboard-course-title">
-                The structure, components, and internal operation of computer systems
-              </p>
-              <button> Go </button>
+          <div className="wd-dashboard-course col" style={{ width: "300px" }}>
+            <div className="card rounded-3 overflow-hidden">
+              <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                    to="/Kanbas/Courses/5600/Home">
+                <img src="/images/5600.jpg" width="100%" height={160}/>
+                <div className="card-body">
+                  <h5 className="wd-dashboard-course-title card-title">
+                  CS5600 Computer Systems	{/* Course 7 */}
+                  </h5>
+                  <p className="wd-dashboard-course-title card-text">
+                  The structure, components, and internal operation of computer systems
+                  </p>
+                  <button className="btn btn-primary"> Go </button>
+                </div>
+              </Link>
             </div>
-          </Link>
+          </div>         
+          
         </div>
-
       </div>
     </div>
   );
