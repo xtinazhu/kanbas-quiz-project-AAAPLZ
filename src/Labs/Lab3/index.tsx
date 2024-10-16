f1bb0029601fa450b1bd751c0f0b7d141189480a
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-16 12:23:42 -0400

    a3 2.2.2

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,10 +1,12 @@
 import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
 
 export default function Lab3() {
   return (
     <div id="wd-lab3">
       <h3>Lab 3</h3>
       <VariablesAndConstants/>
+      <VariableTypes/>
     </div>
   );
 }
