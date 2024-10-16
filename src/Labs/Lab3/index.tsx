806acb8a79dccc3650b70d7effb089868023629e
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-16 13:11:26 -0400

    a3 2.4.6

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,3 +1,4 @@
+//JavaScript Basics
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 import BooleanVariables from "./BooleanVariables";
@@ -5,16 +6,21 @@ import IfElse from "./IfElse";
 import TernaryOperator from "./TernaryOperator";
 import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
 import ConditionalOutputInline from "./ConditionalOutputInline";
+
+//JavaScript Functions
 import LegacyFunctions from "./LegacyFunctions";
 import ArrowFunctions from "./ArrowFunctions";
 import ImpliedReturn from "./ImpliedReturn";
 import TemplateLiterals from "./TemplateLiterals";
+
+//JavaScript Data Structures
 import SimpleArrays from "./SimpleArrays";
 import ArrayIndexAndLength from "./ArrayIndexAndLength";
 import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
 import ForLoops from "./ForLoops";
 import MapFunction from "./MapFunction";
 import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
 
 export default function Lab3() {
   return (
@@ -27,16 +33,19 @@ export default function Lab3() {
       <TernaryOperator/>
       <ConditionalOutputIfElse/>
       <ConditionalOutputInline/>
+
       <LegacyFunctions/>
       <ArrowFunctions/>
       <ImpliedReturn/>
       <TemplateLiterals/>
+      
       <SimpleArrays/>
       <ArrayIndexAndLength/>
       <AddingAndRemovingToFromArrays/>
       <ForLoops/>
       <MapFunction/>
       <FindFunction/>
+      <FindIndex/>
     </div>
   );
 }
