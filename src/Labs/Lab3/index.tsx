568d53a558734473cf92119a2745eae0298055d8
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-16 23:48:26 -0400

    a3 2.6.3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -36,6 +36,7 @@ import Styles from "./Styles";
 import Add from "./Add";
 import Square from "./Square";
 import Highlight from "./Highlight";
+import PathParameters from "./PathParameters";
 
 export default function Lab3() {
   return (
@@ -80,6 +81,7 @@ export default function Lab3() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
         vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
       </Highlight>
+      <PathParameters/>
     </div>
   );
 }
