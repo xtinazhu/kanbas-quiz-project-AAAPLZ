be56b320244b14c41dde25704d05665ac6e441d4
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-17 18:41:42 -0400

    a3 3.7 cid has problem

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -2,84 +2,41 @@ import ModulesControls from "./ModulesControls";
 import ModuleControlButtons from "./ModuleControlButtons";
 import LessonControlButtons from "./LessonControlButtons";
 import { BsGripVertical } from 'react-icons/bs';
+import { useParams } from "react-router";
+import * as db from "../../Database";
 export default function Modules() {
+  const { cid } = useParams();
+  const modules = db.modules;
   return (
     <div>
-      {/*<h2>Modules</h2>
-      <button id="wd-all-good" onClick={() => alert("Life is Good!")} type="button">
-        Collapse All
-      </button> <button id="wd-all-good" onClick={() => alert("Life is Good!")} type="button">
-        View Progress
-      </button> <select id="wd-select-one-genre">
-        <option value="Publish All">Publish All</option>
-        <option value="Publish all modules and items">Publish all modules and items</option>
-        <option value="Publish muduless only">Publish muduless only</option>
-        <option value="Unpublish all modules and items">Unpublish all modules and items</option>
-        <option value="Unpublish modules only">Unpublish modules only</option>
-      </select> <button id="wd-all-good" onClick={() => alert("Life is Good!")} type="button">
-        + Module
-      </button> */}
-
       <ModulesControls /><br /><br /><br /><br />
       <ul id="wd-modules" className="list-group rounded-0">
+        {modules
+            .filter((module: any) => module.course === cid)
+            .map((module: any) => (
         <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
           <div className="wd-title p-3 ps-2 bg-secondary"> 
             <BsGripVertical className="me-2 fs-3" />
-            Week 1
+              {module.name}
             <ModuleControlButtons />
           </div>
+          {module.lessons && (
           <ul className="wd-lessons list-group rounded-0">
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" />
-              LEARNING OBJECTIVES
-              <LessonControlButtons />
-            </li>
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" />
-              Introduction to the course
-              <LessonControlButtons />
-            </li>
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" />
-              Learn what is Web Development
-              <LessonControlButtons />
-            </li>
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" />
-              LESSON 1
-              <LessonControlButtons />
-            </li>
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" />
-              LESSON 2
-              <LessonControlButtons />
-            </li>
+            {module.lessons.map((lesson: any) => (
+              <li className="wd-lesson list-group-item p-3 ps-1">
+                <BsGripVertical className="me-2 fs-3" />
+                {lesson.name}
+                <LessonControlButtons />
+              </li>
+            ))}
+            
+            
           </ul>
+          )}
         </li>
-        <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
-          <div className="wd-title p-3 ps-2 bg-secondary">
-            <BsGripVertical className="me-2 fs-3" />
-            Week 2
-            <ModuleControlButtons />
-          </div>
-          <ul className="wd-lessons list-group rounded-0">
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" />
-              LEARNING OBJECTIVES
-              <LessonControlButtons />
-            </li>
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" />
-              LESSON 1
-              <LessonControlButtons />
-            </li>
-            <li className="wd-lesson list-group-item p-3 ps-1">
-              <BsGripVertical className="me-2 fs-3" />
-              LESSON 2
-              <LessonControlButtons />
-            </li>
-          </ul>
-        </li>
+        ))}
+        
+        
       </ul>
       
   </div>
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -13,7 +13,7 @@ export default function Kanbas() {
           <h5>GitHub: https://github.com/evazhuyh/kanbas-react-web-app-a1.git</h5>
           <h1>Kanbas</h1>
           <Routes>
-              <Route path="/" element={<Navigate to="Dashboard" />} />
+              <Route path="/" element={<Navigate to="Account" />} />
               <Route path="/Account/*" element={<Account />} />
               <Route path="/Dashboard" element={<Dashboard />} />
               <Route path="/Courses/:cid/*" element={<Courses />} />
