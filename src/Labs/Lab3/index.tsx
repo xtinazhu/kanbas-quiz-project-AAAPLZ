9e98c400d5360b60f662b1c6d2dcff9d81756653
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-17 14:10:07 -0400

    a3 2.7.1

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -39,6 +39,7 @@ import Highlight from "./Highlight";
 import PathParameters from "./PathParameters";
 
 export default function Lab3() {
+  console.log('Hello World!');
   return (
     <div id="wd-lab3">
       <h3>Lab 3</h3>
