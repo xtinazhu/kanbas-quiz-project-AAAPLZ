8f0d4ed177924479a65d02e7388f073d9cce4c6b
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-16 23:42:26 -0400

    a3 2.6.2

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -6,37 +6,31 @@ export default function TOC() {
     <ul className="nav nav-pills">
       <li className="nav-item">
         <a id="wd-a" href="#/Labs" className="nav-link">
-          Labs
-        </a>
-      </li>
+          Labs</a></li>
+        
       <li className="nav-item">
         <a id="wd-a1" href="#/Labs/Lab1"
           className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}>
-          Lab 1
-        </a>
-      </li>
+          Lab 1</a></li>
+        
       <li className="nav-item">
         <a id="wd-a2" href="#/Labs/Lab2"
           className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}>
-          Lab 2
-        </a>
-      </li>
+          Lab 2</a></li>
+        
       <li className="nav-item">
         <a id="wd-a3" href="#/Labs/Lab3"
           className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>
-          Lab 3
-        </a>
-      </li>
+          Lab 3</a></li>
+        
       <li className="nav-item">
         <a id="wd-k" href="#/Kanbas" className="nav-link">
-          Kanbas
-        </a>
-      </li>
+          Kanbas</a></li>
+        
       <li className="nav-item">
         <a id="wd-k" href="https://github.com/jannunzi" className="nav-link">
-          My GitHub
-        </a>
-      </li>
+          My GitHub</a></li>
+        
     </ul>
   );
 }
