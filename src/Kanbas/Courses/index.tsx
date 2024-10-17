9f00ba3b26f1e793fae212b0f1eac84d0fa0e0fc
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-17 16:27:42 -0400

    a3 3.4

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,17 +1,20 @@
+import { courses } from "../Database";
 import CoursesNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import PeopleTable from "./People/Table";
-import { Navigate, Route, Routes } from "react-router";
+import { Navigate, Route, Routes , useParams } from "react-router";
 import { FaAlignJustify } from "react-icons/fa";
 export default function Courses() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
       <FaAlignJustify className="me-4 fs-4 mb-1" />
-        Course 1234 </h2> <hr />
+        {course && course.name} </h2> <hr />
         <div className="d-flex">
           <div className="d-none d-md-block">
             <CoursesNavigation />
