59c6bf4c640bea5589d39dfa72ced07871f5ff06
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-17 17:28:42 -0400

    a3 3.5

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,20 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link , useLocation , useParams} from "react-router-dom";
+import * as db from "../Database";
 export default function CoursesNavigation() {
+  const { courseId } = useParams();
+  const { pathname } = useLocation();
+  const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link to="/Kanbas/Courses/1234/Home" id="wd-course-home-link"
-        className="list-group-item active border border-0"> Home </Link>
-      <Link to="/Kanbas/Courses/1234/Modules" id="wd-course-modules-link"
-        className="list-group-item text-danger border border-0"> Modules </Link>
-      <Link to="/Kanbas/Courses/1234/Piazza" id="wd-course-piazza-link"
-        className="list-group-item text-danger border border-0"> Piazza </Link>
-      <Link to="/Kanbas/Courses/1234/Zoom" id="wd-course-zoom-link"
-        className="list-group-item text-danger border border-0"> Zoom </Link>
-      <Link to="/Kanbas/Courses/1234/Assignments" id="wd-course-quizzes-link"
-        className="list-group-item text-danger border border-0"> Assignments </Link>
-      <Link to="/Kanbas/Courses/1234/Quizzes" id="wd-course-assignments-link"
-        className="list-group-item text-danger border border-0"> Quizzes </Link>
-      <Link to="/Kanbas/Courses/1234/People" id="wd-course-people-link"
-        className="list-group-item text-danger border border-0" > People </Link>
+      {links.map((link) => (
+        <Link key={link} to={`/Kanbas/Courses/${courseId}/${link}`}
+          className={`list-group-item border border-0 ${pathname.includes(link) ? "active text-danger" : "text-danger"}` }>
+        {link} </Link>
+      ))}
     </div>
 );}
