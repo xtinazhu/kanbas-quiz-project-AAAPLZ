296a68df8f5ea74a5e5c0b1bf4afcfcd89914a99
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-16 23:17:17 -0400

    a3 2.6.1

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -34,6 +34,8 @@ import DestructingImports from "./DestructingImports";
 import Classes from "./Classes";
 import Styles from "./Styles";
 import Add from "./Add";
+import Square from "./Square";
+import Highlight from "./Highlight";
 
 export default function Lab3() {
   return (
@@ -71,6 +73,13 @@ export default function Lab3() {
       <Classes/>
       <Styles/>
       <Add a={3} b={4} />
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+      </Highlight>
     </div>
   );
 }
