fff3af1e8215d1b3f7a37bc79aeed4450cbb0c76
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-17 18:50:15 -0400

    a3 3.7 cid fix

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,12 +1,12 @@
 import { Link , useLocation , useParams} from "react-router-dom";
 export default function CoursesNavigation() {
-  const { courseId } = useParams();
+  const { cid } = useParams();
   const { pathname } = useLocation();
   const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
       {links.map((link) => (
-        <Link key={link} to={`/Kanbas/Courses/${courseId}/${link}`}
+        <Link key={link} to={`/Kanbas/Courses/${cid}/${link}`}
           className={`list-group-item border border-0 ${pathname.includes(link) ? "active text-danger" : "text-danger"}` }>
         {link} </Link>
       ))}
