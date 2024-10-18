c54453bcbfcc92ed7c6648877dd4645bd3298b1a
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-17 22:32:46 -0400

    a3 3.9

--- src/Kanbas/Database/index.tsx
+++ src/Kanbas/Database/index.tsx
@@ -1,4 +1,6 @@
 import courses from "./courses.json";
 import modules from "./modules.json";
 import assignments from "./assignments.json";
-export { courses, modules , assignments };
+import enrollments from "./enrollments.json";
+import users from "./users.json";
+export { courses, modules , assignments , enrollments , users};
