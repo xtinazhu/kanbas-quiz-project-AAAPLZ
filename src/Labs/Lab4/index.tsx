ec26a9aec9043732324f5155889210cefc1b0b82
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-28 17:41:04 -0400

    a4 2.1

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,8 @@
+export default function Lab4() {
+  return (
+    <div id="wd-lab3">
+      <h3>Lab 4</h3>
+
+    </div>
+  );
+}
--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -22,6 +22,11 @@ export default function TOC() {
         <a id="wd-a3" href="#/Labs/Lab3"
           className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}>
           Lab 3</a></li>
+      
+      <li className="nav-item">
+        <a id="wd-a3" href="#/Labs/Lab4"
+          className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}>
+          Lab 4</a></li>
         
       <li className="nav-item">
         <a id="wd-k" href="#/Kanbas" className="nav-link">
