e12a87558cea99f033790ae0db486c6b58710ac6
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-28 23:00:04 -0400

    a4 2.2.3

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,12 +1,19 @@
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
 
 export default function Lab4() {
+  //passing functions
+  function sayHello() {
+    alert("Hello");
+  }
+
   return (
     <div id="wd-lab3">
       <h3>Lab 4</h3>
       <ClickEvent/>
       <PassingDataOnEvent/>
+      <PassingFunctions theFunction={sayHello} />
     </div>
   );
 }
