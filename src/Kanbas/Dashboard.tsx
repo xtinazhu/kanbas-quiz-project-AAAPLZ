67bc36a98bd46bda2cee27a15c5f19989629b033
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-31 12:18:19 -0400

    a4 3.1.2

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -13,6 +13,9 @@ export default function Dashboard() {
                         _id: new Date().getTime().toString() };
     setCourses([...courses, { ...course, ...newCourse }]);
   };
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
 
   return (
     <div id="wd-dashboard">
@@ -45,6 +48,13 @@ export default function Dashboard() {
                     <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
                       {course.description} </p>
                     <button className="btn btn-primary"> Go </button>
+                    <button onClick={(event) => {
+                              event.preventDefault();
+                              deleteCourse(course._id);
+                            }} className="btn btn-danger float-end"
+                            id="wd-delete-course-click">
+                            Delete
+                    </button>
                   </div>
                 </Link>
               </div>
