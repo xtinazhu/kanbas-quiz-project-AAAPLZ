a4cb9ba64c91d83f2f3b0d4144ee2a8b53daaa8f
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-31 13:33:37 -0400

    a4 3.3.4

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -5,11 +5,18 @@ import { BsGripVertical } from 'react-icons/bs';
 import React, { useState } from "react";
 import { useParams } from "react-router";
 import * as db from "../../Database";
+
+import { addModule, editModule, updateModule, deleteModule }
+  from "./reducer";
+import { useSelector, useDispatch } from "react-redux";
+
 export default function Modules() {
   const { cid } = useParams();
-  const [modules, setModules] = useState<any[]>(db.modules);
   const [moduleName, setModuleName] = useState("");
-  const addModule = () => {
+  const { modules } = useSelector((state: any) => state.modulesReducer);
+  const dispatch = useDispatch();
+
+  /*const addModule = () => {
     setModules([ ...modules, { _id: new Date().getTime().toString(),
                                      name: moduleName, course: cid, lessons: [] } ]);
     setModuleName("");
@@ -22,11 +29,15 @@ export default function Modules() {
   };
   const updateModule = (module: any) => {
     setModules(modules.map((m) => (m._id === module._id ? module : m)));
-  };
+  };*/
 
   return (
     <div>
-      <ModulesControls setModuleName={setModuleName} moduleName={moduleName} addModule={addModule}/><br /><br /><br /><br />
+      <ModulesControls setModuleName={setModuleName} moduleName={moduleName} 
+        addModule={() => {
+          dispatch(addModule({ name: moduleName, course: cid }));
+          setModuleName("");
+        }} /><br /><br /><br /><br />
       <ul id="wd-modules" className="list-group rounded-0">
         {modules
             .filter((module: any) => module.course === cid)
@@ -37,18 +48,21 @@ export default function Modules() {
             {!module.editing && module.name}
             { module.editing && (
               <input className="form-control w-50 d-inline-block"
-                    onChange={(e) => updateModule({ ...module, name: e.target.value })}
+                    onChange={(e) => dispatch(
+                      updateModule({ ...module, name: e.target.value })
+                    )}
                     onKeyDown={(e) => {
                       if (e.key === "Enter") {
-                        updateModule({ ...module, editing: false });
-                      }
-                    }}
-                    defaultValue={module.name}/>
+                        dispatch(updateModule({ ...module, editing: false }));
+                      } }}
+                    defaultValue={module.name} />
             )}
             <ModuleControlButtons 
                 moduleId={module._id}
-                deleteModule={deleteModule}
-                editModule={editModule}/>
+                deleteModule={(moduleId) => {
+                  dispatch(deleteModule(moduleId));
+                }}
+                editModule={(moduleId) => dispatch(editModule(moduleId))}/>
           </div>
           {module.lessons && (
           <ul className="wd-lessons list-group rounded-0">
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -8,6 +8,9 @@ import Courses from "./Courses";
 import * as db from "./Database";
 import { useState } from "react";
 
+import store from "./store";
+import { Provider } from "react-redux";
+
 export default function Kanbas() {
   const [courses, setCourses] = useState<any[]>(db.courses);
   const [course, setCourse] = useState<any>({
@@ -33,29 +36,31 @@ export default function Kanbas() {
   };
 
   return (
-    <div id="wd-kanbas">
-      <KanbasNavigation />
-        <div className="wd-main-content-offset p-3">
-          <h5>Yihua Zhu 20593</h5>
-          <h5>GitHub: https://github.com/evazhuyh/kanbas-react-web-app-a1.git</h5>
-          <h1>Kanbas</h1>
-          <Routes>
-              <Route path="/" element={<Navigate to="Account" />} />
-              <Route path="/Account/*" element={<Account />} />
-              <Route path="Dashboard" element={
-                <Dashboard
-                  courses={courses}
-                  course={course}
-                  setCourse={setCourse}
-                  addNewCourse={addNewCourse}
-                  deleteCourse={deleteCourse}
-                  updateCourse={updateCourse}/>
-              } />
-              <Route path="/Courses/:cid/*" element={<Courses courses={courses} />} />
-              <Route path="/Calendar" element={<h1>Calendar</h1>} />
-              <Route path="/Inbox" element={<h1>Inbox</h1>} />
-              
-            </Routes>
-        </div>
-    </div>
+    <Provider store={store}>
+      <div id="wd-kanbas">
+        <KanbasNavigation />
+          <div className="wd-main-content-offset p-3">
+            <h5>Yihua Zhu 20593</h5>
+            <h5>GitHub: https://github.com/evazhuyh/kanbas-react-web-app-a1.git</h5>
+            <h1>Kanbas</h1>
+            <Routes>
+                <Route path="/" element={<Navigate to="Account" />} />
+                <Route path="/Account/*" element={<Account />} />
+                <Route path="Dashboard" element={
+                  <Dashboard
+                    courses={courses}
+                    course={course}
+                    setCourse={setCourse}
+                    addNewCourse={addNewCourse}
+                    deleteCourse={deleteCourse}
+                    updateCourse={updateCourse}/>
+                } />
+                <Route path="/Courses/:cid/*" element={<Courses courses={courses} />} />
+                <Route path="/Calendar" element={<h1>Calendar</h1>} />
+                <Route path="/Inbox" element={<h1>Inbox</h1>} />
+                
+              </Routes>
+          </div>
+      </div>
+    </Provider>
 );}
