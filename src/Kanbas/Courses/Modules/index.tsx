5e87ce257be4baeb6dd74fe327891c8c0bcadd49
evazhuyh <zhu.yihua@northeastern.edu>
2024-10-31 13:14:07 -0400

    a4 3.3.3

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -17,6 +17,12 @@ export default function Modules() {
   const deleteModule = (moduleId: string) => {
     setModules(modules.filter((m) => m._id !== moduleId));
   };
+  const editModule = (moduleId: string) => {
+    setModules(modules.map((m) => (m._id === moduleId ? { ...m, editing: true } : m)));
+  };
+  const updateModule = (module: any) => {
+    setModules(modules.map((m) => (m._id === module._id ? module : m)));
+  };
 
   return (
     <div>
@@ -28,10 +34,21 @@ export default function Modules() {
         <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
           <div className="wd-title p-3 ps-2 bg-secondary"> 
             <BsGripVertical className="me-2 fs-3" />
-              {module.name}
+            {!module.editing && module.name}
+            { module.editing && (
+              <input className="form-control w-50 d-inline-block"
+                    onChange={(e) => updateModule({ ...module, name: e.target.value })}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        updateModule({ ...module, editing: false });
+                      }
+                    }}
+                    defaultValue={module.name}/>
+            )}
             <ModuleControlButtons 
                 moduleId={module._id}
-                deleteModule={deleteModule}/>
+                deleteModule={deleteModule}
+                editModule={editModule}/>
           </div>
           {module.lessons && (
           <ul className="wd-lessons list-group rounded-0">
