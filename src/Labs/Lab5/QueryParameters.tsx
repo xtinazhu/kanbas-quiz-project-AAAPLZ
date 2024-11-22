43c994fde26204de903644c0321a33bd0429fb8b
evazhuyh <zhu.yihua@northeastern.edu>
2024-11-22 01:15:28 -0500

    a5 3.2.3

--- src/Labs/Lab5/QueryParameters.tsx
+++ src/Labs/Lab5/QueryParameters.tsx
@@ -3,6 +3,8 @@ const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function QueryParameters() {
   const [a, setA] = useState("34");
   const [b, setB] = useState("23");
+  const [c, setC] = useState("10");
+  const [d, setD] = useState("5");
   return (
     <div id="wd-query-parameters">
       <h3>Query Parameters</h3>
@@ -22,7 +24,24 @@ export default function QueryParameters() {
         href={`${REMOTE_SERVER}/lab5/calculator?operation=subtract&a=${a}&b=${b}`}>
         Substract {a} - {b}
       </a>
-      {/* create additional links to test multiply and divide. use IDs starting with wd-query-parameter- */}
+      <hr />
+
+      <input id="wd-query-parameter-c"
+            className="form-control mb-2"
+            defaultValue={c} type="number"
+            onChange={(e) => setC(e.target.value)} />
+      <input id="wd-query-parameter-d"
+            className="form-control mb-2"
+            defaultValue={d} type="number"
+            onChange={(e) => setD(e.target.value)} />
+      <a id="wd-query-parameter-multiply"
+        href={`${REMOTE_SERVER}/lab5/calculator?operation=multiply&c=${c}&d=${d}`}>
+        Multiply {c} * {d}
+      </a>
+      <a id="wd-query-parameter-divide"
+        href={`${REMOTE_SERVER}/lab5/calculator?operation=divide&c=${c}&d=${d}`}>
+        Divide {c} / {d}
+      </a>
       <hr />
     </div>
   );
