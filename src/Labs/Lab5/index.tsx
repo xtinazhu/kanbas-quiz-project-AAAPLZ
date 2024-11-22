6fabef74ad3e1d680fe377a6f1ac8a3fb6764575
evazhuyh <zhu.yihua@northeastern.edu>
2024-11-21 23:31:11 -0500

    a5 3.1

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,13 +1,17 @@
+import EnvironmentVariables from "./EnvironmentVariables";
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+
 export default function Lab5() {
   return (
     <div id="wd-lab5">
       <h2>Lab 5</h2>
       <div className="list-group">
-        <a href="http://localhost:4000/lab5/welcome"          
+        <a href={`${REMOTE_SERVER}/lab5/welcome`}          
            className="list-group-item">
            Welcome
         </a>
       </div><hr/>
+      <EnvironmentVariables />
     </div>
   );
 }
