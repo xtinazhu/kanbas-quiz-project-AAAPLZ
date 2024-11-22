19d4ab6d9a3b102e8d067fe3bedee2b21a29c4cd
evazhuyh <zhu.yihua@northeastern.edu>
2024-11-22 00:17:10 -0500

    a5 3.2.2

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,5 +1,6 @@
 import EnvironmentVariables from "./EnvironmentVariables";
 import PathParameters from "./PathParameters";
+import QueryParameters from "./QueryParameters";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 
 export default function Lab5() {
@@ -14,6 +15,7 @@ export default function Lab5() {
       </div><hr/>
       <EnvironmentVariables />
       <PathParameters />
+      <QueryParameters />
     </div>
   );
 }
