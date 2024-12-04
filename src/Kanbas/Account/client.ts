c76f399ae35af94578acab23dcd6bb08aaadc544
evazhuyh <zhu.yihua@northeastern.edu>
2024-12-04 15:52:37 -0500

    a6 3.6.4

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -3,6 +3,11 @@ const axiosWithCredentials = axios.create({ withCredentials: true });
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 
+export const findUserById = async (id: string) => {
+  const response = await axios.get(`${USERS_API}/${id}`);
+  return response.data;
+};
+
 export const findUsersByPartialName = async (name: string) => {
   const response = await axios.get(`${USERS_API}?name=${name}`);
   return response.data;
--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { FaUserCircle } from "react-icons/fa";
+import PeopleDetails from "./Details";
+import { Link } from "react-router-dom";
+import * as client from "../../Account/client";
 
 export default function PeopleTable({ users = [] }: { users?: any[] }) {
   return (
     <div id="wd-people-table">
+      <PeopleDetails />
       <table className="table table-striped">
         <thead>
           <tr>
@@ -15,9 +19,11 @@ export default function PeopleTable({ users = [] }: { users?: any[] }) {
             .map((user: any) => (
               <tr key={user._id}>
                 <td className="wd-full-name text-nowrap">
-                  <FaUserCircle className="me-2 fs-1 text-secondary" />
-                  <span className="wd-first-name">{user.firstName}</span>
-                  <span className="wd-last-name">{user.lastName}</span>
+                  <Link to={`/Kanbas/Account/Users/${user._id}`} className="text-decoration-none">
+                    <FaUserCircle className="me-2 fs-1 text-secondary" />
+                    <span className="wd-first-name">{user.firstName}</span>{" "}
+                    <span className="wd-last-name">{user.lastName}</span>
+                  </Link>
                 </td>
                 <td className="wd-login-id">{user.loginId}</td>
                 <td className="wd-section">{user.section}</td>
