061eb561b6a23de8d349dd347caf7dc45d97f444
evazhuyh <zhu.yihua@northeastern.edu>
2024-12-06 02:15:22 -0500

    a6 3.6.7

--- src/Kanbas/Account/Users.tsx
+++ src/Kanbas/Account/Users.tsx
@@ -1,12 +1,28 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import { FaPlus } from "react-icons/fa6";
 import PeopleTable from "../Courses/People/Table";
 import * as client from "./client";
+
 export default function Users() {
   const [users, setUsers] = useState<any[]>([]);
   const [role, setRole] = useState("");
   const [name, setName] = useState("");
+  const { uid } = useParams();
 
+  const createUser = async () => {
+    const user = await client.createUser({
+      firstName: "New",
+      lastName: `User${users.length + 1}`,
+      username: `newuser${Date.now()}`,
+      password: "password123",
+      email: `email${users.length + 1}@neu.edu`,
+      section: "S101",
+      role: "STUDENT",
+    });
+    setUsers([...users, user]);
+  };
+  
   const filterUsersByName = async (name: string) => {
     setName(name);
     if (name) {
@@ -16,6 +32,7 @@ export default function Users() {
       fetchUsers();
     }
   };
+
   const filterUsersByRole = async (role: string) => {
     setRole(role);
     if (role) {
@@ -26,26 +43,39 @@ export default function Users() {
     }
   };
 
-  const { uid } = useParams();
   const fetchUsers = async () => {
     const users = await client.findAllUsers();
     setUsers(users);
   };
+
   useEffect(() => {
     fetchUsers();
   }, [uid]);
+
   return (
     <div>
       <h3>Users</h3>
-      <input onChange={(e) => filterUsersByName(e.target.value)} placeholder="Search people"
-             className="form-control float-start w-25 me-2 wd-filter-by-name" />
-
-      <select value={role} onChange={(e) =>filterUsersByRole(e.target.value)}
-              className="form-select float-start w-25 wd-select-role" >
-        <option value="">All Roles</option>    <option value="STUDENT">Students</option>
-        <option value="TA">Assistants</option> <option value="FACULTY">Faculty</option>
+      <button onClick={createUser} className="float-end btn btn-danger wd-add-people">
+        <FaPlus className="me-2" />
+        Users
+      </button>
+      <input 
+        onChange={(e) => filterUsersByName(e.target.value)} 
+        placeholder="Search people"
+        className="form-control float-start w-25 me-2 wd-filter-by-name" 
+      />
+      <select 
+        value={role} 
+        onChange={(e) => filterUsersByRole(e.target.value)}
+        className="form-select float-start w-25 wd-select-role"
+      >
+        <option value="">All Roles</option>
+        <option value="STUDENT">Students</option>
+        <option value="TA">Assistants</option>
+        <option value="FACULTY">Faculty</option>
         <option value="ADMIN">Administrators</option>
       </select>
       <PeopleTable users={users} />
     </div>
-);}
+  );
+}
--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -3,6 +3,10 @@ const axiosWithCredentials = axios.create({ withCredentials: true });
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 
+export const createUser = async (user: any) => {
+  const response = await axios.post(`${USERS_API}`, user);
+  return response.data;
+};
 
 export const deleteUser = async (userId: string) => {
   const response = await axios.delete( `${USERS_API}/${userId}` );
