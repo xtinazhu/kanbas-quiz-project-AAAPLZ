c507a3ff8638bc42c7582865026800f9b15b9b1c
evazhuyh <zhu.yihua@northeastern.edu>
2024-12-08 21:32:46 -0500

    delete lab route in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Labs from "./Labs";
 import Kanbas from "./Kanbas";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
@@ -12,7 +11,6 @@ export default function App() {
         <div>
           <Routes>
             <Route path="/" element={<Navigate to="Kanbas" />} />
-            <Route path="/Labs/*" element={<Labs />} />
             <Route path="/Kanbas/*" element={<Kanbas />} />
           </Routes>
         </div>
